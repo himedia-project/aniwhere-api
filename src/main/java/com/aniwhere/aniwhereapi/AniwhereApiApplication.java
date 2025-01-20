fe36396aedc3856270a85a8e128432d1630ef4dd
seonggon do <mooh2jj@naver.com>
2025-01-20 13:16:26 +0900

    first setting

---
+++ src/main/java/com/aniwhere/aniwhereapi/AniwhereApiApplication.java
@@ -0,0 +1,13 @@
+package com.aniwhere.aniwhereapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniwhereApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniwhereApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aniwhere/aniwhereapi/AniwhereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniwhere.aniwhereapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniwhereApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
