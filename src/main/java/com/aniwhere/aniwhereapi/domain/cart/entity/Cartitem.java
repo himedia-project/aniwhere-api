48c1d9d52b271cfb6700c2acec437272984bb113
minhwan9230 <xxcc96@naver.com>
2025-01-21 10:36:35 +0900

    2

--- src/main/java/com/aniwhere/aniwhereapi/domain/cart/entity/Cartitem.java
+++ src/main/java/com/aniwhere/aniwhereapi/domain/cart/entity/Cartitem.java
@@ -1,4 +1,43 @@
 package com.aniwhere.aniwhereapi.domain.cart.entity;
 
-public class Cartitem {
+import com.aniwhere.aniwhereapi.domain.product.entity.Product;
+import com.aniwhere.aniwhereapi.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+
+@Entity
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"cart"})
+@Table(name = "cart_item", indexes = {
+        @Index(name = "idx_cart_item", columnList = "cart_id"),
+        @Index(name = "idx_cart_item_product", columnList = "product_id, cart_id")
+})
+public class Cartitem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    private int qty;
+
+    public void changeQty(int qty) {
+        this.qty = qty;
+    }
+
 }
---
+++ src/main/java/com/aniwhere/aniwhereapi/domain/order/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.aniwhere.aniwhereapi.domain.order.enums;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
