ba20124591bc41d81176a7cbdfdeee40d3c55b6f
seonggon do <mooh2jj@naver.com>
2025-01-28 11:32:06 +0900

    init 초기화 -> data.sql로 수정

--- src/main/java/com/aniwhere/aniwhereapi/init/NotProd.java
+++ src/main/java/com/aniwhere/aniwhereapi/init/NotProd.java
@@ -10,13 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
-//@Profile({"!prod"})
+@Profile({"test"})
 @RequiredArgsConstructor
 public class NotProd {
 
